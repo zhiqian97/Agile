9d92d4f0128e7d6443d43bd50f3b43f01a53aaaa
User <User@User-PC>
2017-10-30 13:42:05 +0800

    my second-branch commit

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -18,6 +18,8 @@ public class Sample {
         System.out.println("Main");
         System.out.println("second");
         System.out.println("first-branch");
+        System.out.println("second-branch");
+
     }
     
 }
